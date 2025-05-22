844efb4f37c5bb0f272cac41694872b25a1c0f6c
Hector2304 <hectorsilva0423@gmail.com>
2025-05-22 15:04:34 -0600

    Redirrecion de boton "Bloquear"

--- laboratorios-front/pages/superusuario/index.vue
+++ laboratorios-front/pages/superusuario/index.vue
@@ -1,36 +1,47 @@
 <template>
-	<div>
-		<Breadcrumbs id="superusuario" />
+  <div>
+    <Breadcrumbs id="superusuario" />
 
-		<b-row align-v="stretch" align-h="around">
-			<b-col cols="12" md="6" lg="5" xl="4" class="my-1">
-				<SimpleMenuButton
-					icon="person"
-					text="Usuarios"
-					@click.native="$router.push('/usuarios')"
-				/>
-			</b-col>
-			<b-col cols="12" md="6" lg="5" xl="4" class="my-1">
-				<SimpleMenuButton
-					icon="layout-text-sidebar-reverse"
-					text="Catálogos"
-					@click.native="$router.push('/catalogos')"
-				/>
-			</b-col>
-			<b-col cols="12" md="6" lg="5" xl="4" class="my-1">
-				<SimpleMenuButton
-					icon="book"
-					text="Cursos externos"
-					@click.native="$router.push('/externos')"
-				/>
-			</b-col>
-			<b-col cols="12" md="6" lg="5" xl="4" class="my-1">
-				<SimpleMenuButton
-					icon="file-earmark-spreadsheet"
-					text="Reportes"
-					@click.native="$router.push('/reportes')"
-				/>
-			</b-col>
-		</b-row>
-	</div>
+    <b-row align-v="stretch" align-h="around">
+      <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
+        <SimpleMenuButton
+          icon="person"
+          text="Usuarios"
+          @click.native="$router.push('/usuarios')"
+        />
+      </b-col>
+
+      <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
+        <SimpleMenuButton
+          icon="layout-text-sidebar-reverse"
+          text="Catálogos"
+          @click.native="$router.push('/catalogos')"
+        />
+      </b-col>
+
+      <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
+        <SimpleMenuButton
+          icon="book"
+          text="Cursos externos"
+          @click.native="$router.push('/externos')"
+        />
+      </b-col>
+
+      <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
+        <SimpleMenuButton
+          icon="lock"
+            text="Bloquear Horario"
+          @click.native="$router.push('/catalogos/laboratorios')"
+        />
+      </b-col>
+
+      <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
+        <SimpleMenuButton
+          icon="file-earmark-spreadsheet"
+          text="Reportes"
+          @click.native="$router.push('/reportes')"
+        />
+      </b-col>
+    </b-row>
+  </div>
 </template>
