bd5685a4e90113faf5b0db12d988ffb25a256813
Hector2304 <hectorsilva0423@gmail.com>
2025-05-23 10:38:40 -0600

    Descripcion de botones

--- laboratorios-front/pages/superusuario/index.vue
+++ laboratorios-front/pages/superusuario/index.vue
@@ -7,40 +7,55 @@
         <SimpleMenuButton
           icon="person"
           text="Usuarios"
+          v-b-tooltip.hover.top
+          title="Gestiona los usuarios registrados en el sistema"
           @click.native="$router.push('/usuarios')"
         />
+
       </b-col>
 
       <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
         <SimpleMenuButton
           icon="layout-text-sidebar-reverse"
           text="Catálogos"
+          v-b-tooltip.hover.top
+          title="Administra catálogos base como laboratorios, asignaturas, etc."
           @click.native="$router.push('/catalogos')"
         />
+
       </b-col>
 
       <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
         <SimpleMenuButton
           icon="book"
           text="Cursos externos"
+          v-b-tooltip.hover.top
+          title="Registra y consulta cursos impartidos fuera de la facultad"
           @click.native="$router.push('/externos')"
         />
+
       </b-col>
 
       <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
         <SimpleMenuButton
-          icon="lock"
-            text="Bloquear Horario"
+          icon="shield-lock"
+          text="Bloquear Horario"
+          v-b-tooltip.hover.top
+          title="Establece horarios no disponibles en los laboratorios"
           @click.native="$router.push('/catalogos/laboratorios')"
         />
+
       </b-col>
 
       <b-col cols="12" md="6" lg="5" xl="4" class="my-1">
         <SimpleMenuButton
           icon="file-earmark-spreadsheet"
           text="Reportes"
+          v-b-tooltip.hover.top
+          title="Consulta reportes sobre laboratorios, usuarios y horarios"
           @click.native="$router.push('/reportes')"
         />
+
       </b-col>
     </b-row>
   </div>
